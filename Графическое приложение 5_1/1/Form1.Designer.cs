c8726d638258b57007f38e5e49a228b44a4030f7
VictoriaKuzminova <119403787+VictoriaKuzminova@users.noreply.github.com>
2022-12-06 14:24:35 +0300

    Add files via upload

---
+++ Графическое приложение 5_1/1/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿
+namespace _1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.H_TextBox = new System.Windows.Forms.TextBox();
+            this.start_TextBox = new System.Windows.Forms.TextBox();
+            this.finish_TextBox = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.res_TextBox = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(34, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(366, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Введите начальную точку диапазона: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(34, 107);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(356, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Введите конечную точку диапазона: ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(34, 174);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Шаг:";
+            // 
+            // H_TextBox
+            // 
+            this.H_TextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.H_TextBox.Location = new System.Drawing.Point(120, 171);
+            this.H_TextBox.Name = "H_TextBox";
+            this.H_TextBox.Size = new System.Drawing.Size(159, 30);
+            this.H_TextBox.TabIndex = 3;
+            // 
+            // start_TextBox
+            // 
+            this.start_TextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.start_TextBox.Location = new System.Drawing.Point(432, 38);
+            this.start_TextBox.Name = "start_TextBox";
+            this.start_TextBox.Size = new System.Drawing.Size(159, 30);
+            this.start_TextBox.TabIndex = 4;
+            // 
+            // finish_TextBox
+            // 
+            this.finish_TextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.finish_TextBox.Location = new System.Drawing.Point(432, 104);
+            this.finish_TextBox.Name = "finish_TextBox";
+            this.finish_TextBox.Size = new System.Drawing.Size(159, 30);
+            this.finish_TextBox.TabIndex = 5;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(623, 64);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(120, 36);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(612, 166);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(148, 46);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Вывести";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // res_TextBox
+            // 
+            this.res_TextBox.Location = new System.Drawing.Point(39, 236);
+            this.res_TextBox.Name = "res_TextBox";
+            this.res_TextBox.Size = new System.Drawing.Size(721, 190);
+            this.res_TextBox.TabIndex = 8;
+            this.res_TextBox.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.res_TextBox);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.finish_TextBox);
+            this.Controls.Add(this.start_TextBox);
+            this.Controls.Add(this.H_TextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Исключение";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox H_TextBox;
+        private System.Windows.Forms.TextBox start_TextBox;
+        private System.Windows.Forms.TextBox finish_TextBox;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox res_TextBox;
+    }
+}
+
---
+++ Графическое приложение 5_1/1/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _1
+{
+    public partial class Form1 : Form
+    {
+        private double a;
+        private double b;
+        private double h;
+
+        private double f(double x)
+        {
+            try
+            {
+                if ((x <= -2) || (x >= 2)) throw new Exception();
+                else return Math.Log(4 - Math.Pow(x, 2));
+            }
+            catch
+            {
+                throw;
+            }
+
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (start_TextBox.Text == "" || finish_TextBox.Text == "" || H_TextBox.Text == "")
+            {
+                MessageBox.Show("Введены не все данные!!!");
+            }
+            else
+            {
+                res_TextBox.Text = "";
+                try
+                {
+                    a = double.Parse(start_TextBox.Text);
+                }
+                catch
+                {
+                    MessageBox.Show("Некорректный ввод!");
+                    return;
+                }
+                try
+                {
+                    b = double.Parse(finish_TextBox.Text);
+                }
+                catch
+                {
+                    MessageBox.Show("Некорректный ввод!");
+                    return;
+                }
+                if (a > b)
+                {
+                    MessageBox.Show("Начальная точка диапазона не может быть больше конечной точки");
+                    return;
+                }
+                try
+                {
+                    h = double.Parse(H_TextBox.Text);
+                    if (h == 0)
+                    {
+                        MessageBox.Show("Шаг не может быть равен 0");
+                        return;
+                    }
+                    else if (h < 0)
+                    {
+                        MessageBox.Show("Шаг не может быть меньше 0");
+                        return;
+
+                    }
+                }
+                catch
+                {
+                    Console.Write("Некорректный ввод!");
+                }
+                for (double i = a; i <= b; i += h)
+                {
+                    try
+                    {
+                        res_TextBox.Text += "y(" + Math.Round(i, 2) + ") = " + Math.Round(f(i), 2) + "\n";
+                    }
+                    catch
+                    {
+                        res_TextBox.Text += "y(" + Math.Round(i, 2) + ") = Не определен " + "\n";
+                    }
+
+                }
+
+            }
+        }
+    }
+}
